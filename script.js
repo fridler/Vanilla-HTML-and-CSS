9ca1c8ef6014b14c021020bec2f4e879539dbee9
fridler <fridler@gmail.com>
2021-03-03 10:35:44 +0200

    change image few seconds

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+var images = [];
+
+images[0] = "images/250X110571325734.jpg";
+images[1] = "images/250X110668568400.jpg";
+images[2] = "images/banner_atar.jpg";
+images[3] = "images/paz_250x110059890678.jpg";
+images[4] = "images/paz_250x110103131473.jpg";
+var index = 0;
+
+function changeImage() {
+  document.getElementById("changeImage").src = images[index];
+  if (index == 4) {
+    index = 0;
+  } else {
+    index++;
+  }
+  
+}
+
+setInterval(changeImage,3000)
